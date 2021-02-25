6aaacec6ef42abb97454f0365ea40f23b888bf09
Nick <untwsky@yandex.ru>
2021-02-25 21:03:50 +0300

    Initial Commit

---
+++ 3d earth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3d earth
+//
+//  Created by user on 25.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
